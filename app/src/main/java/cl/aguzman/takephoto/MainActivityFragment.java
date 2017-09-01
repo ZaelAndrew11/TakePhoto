fc36e4761118595ed0cba6c20429977c732cb4ca
Alfonso <legion1193@gmail.com>
2017-09-01 01:17:23 -0300

    First commit

---
+++ app/src/main/java/cl/aguzman/takephoto/MainActivityFragment.java
@@ -0,0 +1,118 @@
+package cl.aguzman.takephoto;
+
+import android.Manifest;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.frosquivel.magicalcamera.MagicalCamera;
+import com.frosquivel.magicalcamera.MagicalPermissions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.Map;
+
+import static android.R.attr.path;
+import static android.R.attr.visible;
+import static android.app.Activity.RESULT_OK;
+import static android.os.Build.VERSION_CODES.M;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    private MagicalPermissions magicalPermissions;
+    private int RESIZE_PHOTO_PIXELS_PERCENTAGE = 45;
+    private MagicalCamera magicalCamera;
+    private ImageView imageView;
+    private Button buttonsave;
+
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        imageView = (ImageView) view.findViewById(R.id.photoIv);
+        buttonsave = (Button) view.findViewById(R.id.savePhotoBtn);
+        String[] permissions = new String[]{
+                Manifest.permission.CAMERA,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        };
+        magicalPermissions = new MagicalPermissions(this, permissions);
+        magicalCamera = new MagicalCamera(getActivity(), RESIZE_PHOTO_PIXELS_PERCENTAGE, magicalPermissions);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Map<String, Boolean> map = magicalPermissions.permissionResult(requestCode, permissions, grantResults);
+        for (String permission : map.keySet()) {
+            Log.d("PERMISSIONS", permission + " was: " + map.get(permission));
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        magicalCamera.resultPhoto(requestCode, resultCode, data);
+        if (RESULT_OK == resultCode) {
+            Bitmap photo = magicalCamera.getPhoto();
+            final String path = magicalCamera.savePhotoInMemoryDevice(photo, "photo", "photos", MagicalCamera.JPEG, true);
+            if (path != null) {
+                imageView.setImageBitmap(photo);
+                buttonsave.setVisibility(View.VISIBLE);
+                buttonsave.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        uploadPhoto(path);
+                    }
+                });
+                Toast.makeText(getContext(), "Foto guardada: " + path, Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(getContext(), "Un error ha ocurrido.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void takePhoto() {
+        magicalCamera.takeFragmentPhoto(this);
+    }
+
+    public void uploadPhoto(String path){
+        path = "file://"+path;
+        String url = "gs://destroy-25ff4.appspot.com/photos/filePhoto.jpg";
+        FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
+        StorageReference storageReference = firebaseStorage.getReferenceFromUrl(url);
+        storageReference.putFile(Uri.parse(path)).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                @SuppressWarnings("VisibleForTests") String url = taskSnapshot.getDownloadUrl().toString();
+                Toast.makeText(getContext(), "Foto Subida", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
